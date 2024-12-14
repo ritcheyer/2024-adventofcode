9e881f3388876bc291ac880046d5cbca2fa0679b
Eric Ritchey <eritchey@indeed.com>
2024-12-14 11:35:11 -0800

    adding day 2

---
+++ 02/01.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const IS_TEST = true;
+
+// turn the data into an array
+if (IS_TEST) {
+  dataFile = `${__dirname}/data/test.txt`;
+} else {
+  dataFile = `${__dirname}/data/input.txt`;
+}
+
+const datum = fs.readFileSync(dataFile, 'utf8').split('\n')
